7a6802c3d16fd4f9846a6681254b48a2c2fd2c43
aljrahmrw9-design <aljrahmrw9@gmail.com>
2026-04-16 14:33:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
 
 def add_numbers(a, b):
     return a + b
 
 @app.route("/")
 def home():
-    return jsonify({"status": "ok", "message": "Hello from gp1-demo!"})
+    return jsonify({
+        "status": "ok",
+        "message": "Use /add?a=2&b=3 to add two numbers"
+    })
+
+@app.route("/add")
+def add():
+    a = request.args.get("a", type=float)
+    b = request.args.get("b", type=float)
+
+    if a is None or b is None:
+        return jsonify({
+            "status": "error",
+            "message": "Please provide two numbers in the URL like /add?a=2&b=3"
+        }), 400
+
+    result = add_numbers(a, b)
+
+    return jsonify({
+        "status": "ok",
+        "a": a,
+        "b": b,
+        "result": result
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
